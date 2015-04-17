8cc981985fdc952e83dc5dbd4d32eff4450b9633
Eric Hsu <erichsu7@gmail.com>
2015-04-16 22:47:28 -0700

    Add push

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,8 +1,31 @@
-require 'node'
+require_relative 'node'
 
 class LinkedList
+  attr_accessor :head, :tail
+
   def initialize
     @head = nil
     @tail = nil
+  end
+
+  def push(value)
+    node = Node.new(value, nil)
+
+    if head.nil?
+      @head = node
+      @tail = node
+    else
+      @tail.next = node
+      @tail = node
+    end
+  end
+
+  def print
+    current_node = @head
+
+    until current_node.nil?
+      puts current_node.value
+      current_node = current_node.next
+    end
   end
 end
--- linked_list/node.rb
+++ linked_list/node.rb
@@ -1,4 +1,6 @@
 class Node
+  attr_accessor :value, :next
+
   def initialize(value, next_node = nil)
     @value = value
     @next = next_node
