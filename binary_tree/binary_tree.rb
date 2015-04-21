a8e77e63b91867039e61a13f816e9bf140b79f0f
Eric Hsu <erichsu7@gmail.com>
2015-04-20 18:00:46 -0700

    Break out tree node into its own class: tree_node.rb

--- binary_tree/binary_tree.rb
+++ binary_tree/binary_tree.rb
@@ -6,4 +6,20 @@ class BinaryTree
     @left = left
     @right = right
   end
+
+  def insert(value)
+    current_node = self
+    until current_node.nil?
+      if value < current_node.value
+        if current_node.left
+          current_node = current_node.left
+        else
+          current_node.left = BinaryTree.new(value)
+        end
+      elsif value > current_node.value
+      else
+        return
+      end
+    end
+  end
 end
---
+++ binary_tree/tree_node.rb
@@ -0,0 +1,6 @@
+class TreeNode
+  attr_accessor :value, :left, :right
+
+  def initialize(value = nil, left = nil, right = nil)
+  end
+end
