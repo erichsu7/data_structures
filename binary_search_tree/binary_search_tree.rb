218e8a5bd6e13eead25b44a063171ed1d43388a2
Eric Hsu <erichsu7@gmail.com>
2015-04-20 19:22:46 -0700

    Write insert for BST

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -6,4 +6,28 @@ class BinarySearchTree < BinaryTree
     super(root)
   end
 
+  def insert(value)
+    unless root
+      self.root = TreeNode.new(value)
+      return value
+    end
+
+    current_node = root
+    until current_node.nil?
+      if value < current_node.value && current_node.left.nil?
+        current_node.left = TreeNode.new(value)
+        return value
+      elsif value > current_node.value && current_node.right.nil?
+        current_node.right = TreeNode.new(value)
+        return value
+      elsif value < current_node.value
+        current_node = current_node.left
+      elsif value > current_node.value
+        current_node = current_node.right
+      else
+        return false
+      end
+    end
+  end
+
 end
