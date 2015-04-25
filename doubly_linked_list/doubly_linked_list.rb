573166a3b33b8dd734b36c38d27c27aebdf8fc60
Eric Hsu <erichsu7@gmail.com>
2015-04-25 00:23:37 -0700

    Add push

--- doubly_linked_list/doubly_linked_list.rb
+++ doubly_linked_list/doubly_linked_list.rb
@@ -8,4 +8,50 @@ class DoublyLinkedList
     @tail = Node.new(nil, nil, head)
     @head.next = tail
   end
+
+  def push(value)
+    node = Node.new(value, tail, tail.prev)
+    tail.prev.next = node
+    tail.prev = node
+
+    value
+  end
+
+  def pop
+  end
+
+  def unshift(value)
+  end
+
+  def shift
+
+  end
+
+  def insert_after
+  end
+
+  def insert_before
+  end
+
+  def insert_at
+  end
+
+  def delete
+  end
+
+  def reverse!
+  end
+
+  def inspect
+    values = []
+    current_node = head.next
+
+    until current_node == tail
+      values << current_node.value
+      current_node = current_node.next
+    end
+
+    values.join(" ")
+  end
+
 end
