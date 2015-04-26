232ebc9f7f2f17c2bface2704d54c3825a440865
Eric Hsu <erichsu7@gmail.com>
2015-04-25 21:11:07 -0700

    Add insert and h sift_up

--- max_heap/max_heap.rb
+++ max_heap/max_heap.rb
@@ -6,19 +6,37 @@ class MaxHeap
     @count = 0
   end
 
-  def left_child(idx)
+  def get_left_child_idx(idx)
     return if (idx * 2) + 1 > count
-    array[(idx * 2) + 1]
+    (idx * 2) + 1
   end
 
-  def right_child(idx)
+  def get_right_child_idx(idx)
     return if (idx * 2) + 2 > count
-    array[(idx * 2) + 2]
+    (idx * 2) + 2
   end
 
-  def parent(idx)
+  def get_parent_idx(idx)
     return if idx < 1 || idx > count
-    array[(idx - 1) / 2]
+    (idx - 1) / 2
+  end
+
+  def insert(value)
+    array[count] = value
+    sift_up(count) if count > 0
+    self.count += 1
+    value
+  end
+
+  def sift_up(idx)
+    parent_idx = get_parent_idx(idx)
+    while !parent_idx.nil? && array[idx] > array[parent_idx]
+      array[idx], array[parent_idx] = array[parent_idx], array[idx]
+      idx = parent_idx
+      parent_idx = get_parent_idx(idx)
+    end
+
+    nil
   end
 
 end
