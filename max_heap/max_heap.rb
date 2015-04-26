5dc1f78cc8b11935ad88dacae143fbe61b7bc27d
Eric Hsu <erichsu7@gmail.com>
2015-04-25 20:55:33 -0700

    Add right child, left child, and parent lookups

--- max_heap/max_heap.rb
+++ max_heap/max_heap.rb
@@ -1,9 +1,24 @@
 class MaxHeap
-  attr_reader :array, :count
+  attr_accessor :array, :count
 
   def initialize
     @array = []
     @count = 0
+  end
+
+  def left_child(idx)
+    return if (idx * 2) + 1 > count
+    array[(idx * 2) + 1]
+  end
+
+  def right_child(idx)
+    return if (idx * 2) + 2 > count
+    array[(idx * 2) + 2]
+  end
+
+  def parent(idx)
+    return if idx < 1 || idx > count
+    array[(idx - 1) / 2]
   end
 
 end
